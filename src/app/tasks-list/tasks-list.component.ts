f99ef7d432e95f3befc11edbf427fe9d6324877b
Usama Subhani <reachusama@protonmail.com>
2020-02-07 22:58:42 +0500

    Added Tasks Component

---
+++ src/app/tasks-list/tasks-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrls: ['./tasks-list.component.css']
+})
+export class TasksListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
