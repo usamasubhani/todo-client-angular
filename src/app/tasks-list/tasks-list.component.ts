2ea2561d19dc95222cc4cbad36ba4a395d0184f9
Usama Subhani <reachusama@protonmail.com>
2020-02-08 16:27:01 +0500

    Display Tasks

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from '../interfaces/task';
 
 @Component({
   selector: 'tasks-list',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+  tasks: Task[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.tasks = [
+      {
+        'id': 1,
+        'title': 'Test Task',
+        'description': 'Test desc',
+        'done': false
+      },
+      {
+        'id': 2,
+        'title': 'Another',
+        'description': 'Test desc 2',
+        'done': true
+      }
+    ];
+  }
+
+  getTasks(): Task[] {
+    return this.tasks;
   }
 
 }
