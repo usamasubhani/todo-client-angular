9943d93d5d32dffdd9c7440f0290dce46e638faf
Usama Subhani <reachusama@protonmail.com>
2020-02-10 19:28:49 +0500

    Retrieve and Show Tasks from api

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../interfaces/task';
+import { DataService } from '../data.service';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'tasks-list',
@@ -7,29 +10,39 @@ import { Task } from '../interfaces/task';
   styleUrls: ['./tasks-list.component.css']
 })
 export class TasksListComponent implements OnInit {
+  // tasks: Observable<Task[]>;
   tasks: Task[];
 
-  constructor() { }
+  constructor(private dataService: DataService) {
+    // this.tasks = this.getTasks();
+   }
 
-  ngOnInit(): void {
-    this.tasks = [
-      {
-        'id': 1,
-        'title': 'Test Task',
-        'description': 'Test desc',
-        'done': false
-      },
-      {
-        'id': 2,
-        'title': 'Another',
-        'description': 'Test desc 2',
-        'done': true
-      }
-    ];
+  ngOnInit() {
+    // this.tasks = this.getTasks();
+    return this.dataService.getTasks()
+     .subscribe(data => this.tasks = data);
   }
 
-  getTasks(): Task[] {
-    return this.tasks;
+  getTasks(){
+    // console.log(this.http.get<Task[]>('http://127.0.0.1:5000/todo/api/v1.0/tasks'));
+    // this.http.get('http://0.0.0.0:5000/todo/api/v1.0/tasks')
+    //  .subscribe((response: Task[]) => {
+    //    this.tasks = response;
+    //    console.log(this.tasks)
+    //  })
+    // return this.tasks;
+    // this.tasks = this.http.get<Task[]>('http://0.0.0.0:5000/todo/api/v1.0/tasks')
+    // return this.http.get<Task[]>('http://0.0.0.0:5000/todo/api/v1.0/tasks/1')
+    //  .subscribe(data => this.tasks = data);
+    // console.log(this.tasks)
+    // .pipe(
+    //   tap(_ => this.log('fetched heroes')),
+    //   catchError(this.handleError<Hero[]>('getHeroes', []))
+  }
+
+
+  showTasks() {
+    console.log(this.tasks)
   }
 
 }
